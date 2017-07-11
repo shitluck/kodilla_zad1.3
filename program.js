b6b68c82b1aee0d5dad5dc1eb5a5cb0fcfc31ee0
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-07-11 11:30:07 +0200

    add files

---
+++ program.js
@@ -0,0 +1,21 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                break;
+            case '/version':
+                console.log(process.version);
+                break;
+            case '/language':
+                console.log(process.env);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
